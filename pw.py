00bee7ac960b29af09ad79d0e9c20bf7a4c7ccb5
Philip Noble <thesuperchimp@googlemail.com>
2019-12-29 00:29:08 +0100

    Push!

--- pw.py
+++ pw.py
@@ -1,8 +1,18 @@
 #! python3
 # A very insure password manager
-import sys
+import sys, pyperclip
 
 passwords = {"email": "password1", 
              "steam":"password2", 
              "airline": "password3"}
 
+if len(sys.argv) < 2:
+    print("Usage: python pw.py [account] - copy account password")
+    sys.exit()
+account = sys.argv[1]       # First command line arg is the account name
+
+if account in passwords:
+    pyperclip.copy(passwords[account])
+    print("Password for " + account + " copied to clipboard.")
+else:
+    print("There is no account by the name of: " + account)
