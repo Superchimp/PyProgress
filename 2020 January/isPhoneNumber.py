b9a54a2346cd45c0d72a7016ac9655196983374e
Philip Noble <thesuperchimp@googlemail.com>
2020-01-16 14:43:05 +0000

    Typo Fix

--- 2020 January/isPhoneNumber.py
+++ 2020 January/isPhoneNumber.py
@@ -5,7 +5,7 @@ def isPhoneNumber(text):
     if len(text) != 12:
         return False
     for i in range(0,3):
-        if not text[0].isdecimal():
+        if not text[i].isdecimal():
             return False
     if text[3] != "-":
         return False
